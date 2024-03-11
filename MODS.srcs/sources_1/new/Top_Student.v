b6c63d58ef31581a8853cbffbddeb56603747569
ngxzs <seanng01@outlook.com>
2024-03-11 23:41:41 +0800

    Add 4.D and .gitignore file

--- MODS.srcs/sources_1/new/Top_Student.v
+++ MODS.srcs/sources_1/new/Top_Student.v
@@ -13,6 +13,7 @@
 
 module Top_Student (
     input clk,
+    input btnC, // task D
     input [15:0] sw,
     output [15:0] led,
     output [6:0] seg,
@@ -74,7 +75,7 @@ module Top_Student (
     
     //  inputs
     reg reset = 0;
-    reg [15:0] pixel_data;
+    wire [15:0] pixel_data; // conflict w line 195
     
     //  outputs
     wire frame_begin;
@@ -161,7 +162,7 @@ module Top_Student (
     
     // ---------- paint ----------
     //  inputs
-    reg enable = 0;
+    reg enable = 0; // disable = 0
     
     //  outputs
     wire [15:0] colour_chooser;
@@ -189,133 +190,138 @@ module Top_Student (
 //        pixel_data <= colour_chooser;
 //    end
     
+    // ---------- 4.D ----------
+    // inputs
+//    wire [15:0] pixel_data; // conflict w line 77
+    taskD unit4(.clk_6p25m(clk_6p25m), .pixel_data(pixel_data), .pixel_index(pixel_index), .btnC(btnC)); 
+    
     //  ---------- 4.B ----------
-    reg[2:0] sw0_counter = 3'b000;
+//    reg[2:0] sw0_counter = 3'b000;
     
-    always @ (posedge clk_1)
-    begin
-        if (sw[0] == 1)
-            begin
-                if (sw0_counter < 3'b100)
-                    begin
-                        sw0_counter <= sw0_counter + 1;
-                    end
-            end
-        else
-        begin
-            sw0_counter = 0;
-        end
-    end
+//    always @ (posedge clk_1)
+//    begin
+//        if (sw[0] == 1)
+//            begin
+//                if (sw0_counter < 3'b100)
+//                    begin
+//                        sw0_counter <= sw0_counter + 1;
+//                    end
+//            end
+//        else
+//        begin
+//            sw0_counter = 0;
+//        end
+//    end
     
-    reg [3:0] box_length = 6;
-//    reg [3:0] box_separation = 11;
-    function is_box;
-        input [7:0] box_center_x;
-        input [7:0] box_center_y;
-        input [7:0] curr_x;
-        input [7:0] curr_y;
-        begin
-            if ((curr_y > box_center_y - box_length / 2) &&
-                (curr_y <= box_center_y + box_length / 2) &&
-                (curr_x > box_center_x - box_length / 2) &&
-                (curr_x <= box_center_x + box_length / 2))
-                is_box = 1;
-            else
-                is_box = 0;
-        end
-    endfunction
+//    reg [3:0] box_length = 6;
+////    reg [3:0] box_separation = 11;
+//    function is_box;
+//        input [7:0] box_center_x;
+//        input [7:0] box_center_y;
+//        input [7:0] curr_x;
+//        input [7:0] curr_y;
+//        begin
+//            if ((curr_y > box_center_y - box_length / 2) &&
+//                (curr_y <= box_center_y + box_length / 2) &&
+//                (curr_x > box_center_x - box_length / 2) &&
+//                (curr_x <= box_center_x + box_length / 2))
+//                is_box = 1;
+//            else
+//                is_box = 0;
+//        end
+//    endfunction
     
-    reg [3:0] border_thickness = 3;
-    reg [3:0] border_distance = 2;
-    function is_border;
-        input [7:0] border_center_x;
-        input [7:0] border_center_y;
-        input [7:0] curr_x;
-        input [7:0] curr_y;
-        reg [7:0] outer_min_x, outer_max_x, outer_min_y, outer_max_y;
-        reg [7:0] inner_min_x, inner_max_x, inner_min_y, inner_max_y;
-        begin
-            outer_min_x = border_center_x - box_length / 2 - border_distance - border_thickness;
-            outer_max_x = border_center_x + box_length / 2 + border_distance + border_thickness;
-            outer_min_y = border_center_y - box_length / 2 - border_distance - border_thickness;
-            outer_max_y = border_center_y + box_length / 2 + border_distance + border_thickness;
+//    reg [3:0] border_thickness = 3;
+//    reg [3:0] border_distance = 2;
+//    function is_border;
+//        input [7:0] border_center_x;
+//        input [7:0] border_center_y;
+//        input [7:0] curr_x;
+//        input [7:0] curr_y;
+//        reg [7:0] outer_min_x, outer_max_x, outer_min_y, outer_max_y;
+//        reg [7:0] inner_min_x, inner_max_x, inner_min_y, inner_max_y;
+//        begin
+//            outer_min_x = border_center_x - box_length / 2 - border_distance - border_thickness;
+//            outer_max_x = border_center_x + box_length / 2 + border_distance + border_thickness;
+//            outer_min_y = border_center_y - box_length / 2 - border_distance - border_thickness;
+//            outer_max_y = border_center_y + box_length / 2 + border_distance + border_thickness;
             
-            inner_min_x = border_center_x - box_length / 2 - border_distance;
-            inner_max_x = border_center_x + box_length / 2 + border_distance;
-            inner_min_y = border_center_y - box_length / 2 - border_distance;
-            inner_max_y = border_center_y + box_length / 2 + border_distance;
+//            inner_min_x = border_center_x - box_length / 2 - border_distance;
+//            inner_max_x = border_center_x + box_length / 2 + border_distance;
+//            inner_min_y = border_center_y - box_length / 2 - border_distance;
+//            inner_max_y = border_center_y + box_length / 2 + border_distance;
             
-            if (curr_y > outer_min_y && curr_y <= inner_min_y)
-                begin
-                    if (curr_x > outer_min_x && curr_x <= outer_max_x)
-                        is_border = 1;
-                    else
-                        is_border = 0;
-                end
-            else if (curr_y > inner_min_y && curr_y <= inner_max_y)
-                begin
-                    if (curr_x > outer_min_x && curr_x <= inner_min_x)
-                        is_border = 1;
-                    else if (curr_x > inner_max_x && curr_x <= outer_max_x)
-                        is_border = 1;
-                    else
-                        is_border = 0;
-                end
-            else if (curr_y > inner_max_y && curr_y <= outer_max_y)
-                begin
-                    if (curr_x > outer_min_x && curr_x <= outer_max_x)
-                        is_border = 1;
-                    else
-                        is_border = 0;
-                end
-            else
-                is_border = 0;
-        end
-    endfunction
+//            if (curr_y > outer_min_y && curr_y <= inner_min_y)
+//                begin
+//                    if (curr_x > outer_min_x && curr_x <= outer_max_x)
+//                        is_border = 1;
+//                    else
+//                        is_border = 0;
+//                end
+//            else if (curr_y > inner_min_y && curr_y <= inner_max_y)
+//                begin
+//                    if (curr_x > outer_min_x && curr_x <= inner_min_x)
+//                        is_border = 1;
+//                    else if (curr_x > inner_max_x && curr_x <= outer_max_x)
+//                        is_border = 1;
+//                    else
+//                        is_border = 0;
+//                end
+//            else if (curr_y > inner_max_y && curr_y <= outer_max_y)
+//                begin
+//                    if (curr_x > outer_min_x && curr_x <= outer_max_x)
+//                        is_border = 1;
+//                    else
+//                        is_border = 0;
+//                end
+//            else
+//                is_border = 0;
+//        end
+//    endfunction
     
-    reg [31:0] x, y;
-    reg [31:0] border_center_x, border_center_y;
-    always @ (posedge clk_25m)
-    begin
-        x = pixel_index % 96;
-        y = pixel_index / 96;
+//    reg [31:0] x, y;
+//    reg [31:0] border_center_x, border_center_y;
+//    always @ (posedge clk_25m)
+//    begin
+//        x = pixel_index % 96;
+//        y = pixel_index / 96;
         
-        if (sw0_counter == 3'b100)
-            begin
-                border_center_x = 70;
-                border_center_y = 32;
+//        if (sw0_counter == 3'b100)
+//            begin
+//                border_center_x = 70;
+//                border_center_y = 32;
                 
-                if (is_border(border_center_x, border_center_y, x, y))
-                    begin
-                        pixel_data <= green;
-                    end
-                else if (is_box(14, 32, x, y) ||
-                        is_box(28, 32, x, y) ||
-                        is_box(42, 32, x, y) ||
-                        is_box(56, 32, x, y) ||
-                        is_box(70, 32, x, y))
-                    begin
-                        pixel_data <= white;
-                    end
-                else
-                    begin
-                        pixel_data <= black;
-                    end
-            end
-        else
-            begin
-                border_center_x = 48;
-                border_center_y = 32;
+//                if (is_border(border_center_x, border_center_y, x, y))
+//                    begin
+//                        pixel_data <= green;
+//                    end
+//                else if (is_box(14, 32, x, y) ||
+//                        is_box(28, 32, x, y) ||
+//                        is_box(42, 32, x, y) ||
+//                        is_box(56, 32, x, y) ||
+//                        is_box(70, 32, x, y))
+//                    begin
+//                        pixel_data <= white;
+//                    end
+//                else
+//                    begin
+//                        pixel_data <= black;
+//                    end
+//            end
+//        else
+//            begin
+//                border_center_x = 48;
+//                border_center_y = 32;
                 
-                if (is_border(border_center_x, border_center_y, x, y))
-                    begin
-                        pixel_data <= green;
-                    end
-                else
-                    begin
-                        pixel_data <= black;
-                    end
-            end
-    end
+//                if (is_border(border_center_x, border_center_y, x, y))
+//                    begin
+//                        pixel_data <= green;
+//                    end
+//                else
+//                    begin
+//                        pixel_data <= black;
+//                    end
+//            end
+//    end
     
 endmodule
